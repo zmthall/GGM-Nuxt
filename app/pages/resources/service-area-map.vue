abb392d98809855074443da69f7ca64b2199bf05
Zachary M Thallas <zmthall@gmail.com>
2025-06-01 22:17:18 -0600

    Updated Service map page back to original state

--- app/pages/resources/service-area-map.vue
+++ app/pages/resources/service-area-map.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>temp</div>
+    <div>
+        temp
+    </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang='ts'> 
+definePageMeta({
+  title: 'Transportation Area Service Map',
+  breadcrumbLabel: 'Service Map'
+})
 
+const runtimeConfig = useRuntimeConfig()
+useSeoMeta({
+  title: 'Transportation Area Service Map',
+  ogTitle: 'Transportation Area Service Map',
+  description: 'Easily see where our transportation services are able to service by using our interactive Colroado county map.',
+  ogDescription: 'Easily see where our transportation services are able to service by using our interactive Colroado county map.',
+  ogImage: `${runtimeConfig.public.siteUrl}/images/seo/ogImage-golden-gate-manor.png`,
+  twitterTitle: 'Schedule Your Transportation',
+  twitterDescription: 'Easily see where our transportation services are able to service by using our interactive Colroado county map.',
+  twitterImage: `${runtimeConfig.public.siteUrl}/images/seo/ogImage-golden-gate-manor.png`,
+  twitterCard: 'summary_large_image',
+})
+
+defineOptions({
+    name: 'ServiceAreaMapPage'
+})
 </script>
 
-<style>
-
-</style>
+<style></style>
