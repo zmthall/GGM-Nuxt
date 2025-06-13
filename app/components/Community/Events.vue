e2f31312bb3e25f210d9cbf3f5bdb69a483b2a6b
zmthall <zmthall@gmail.com>
2025-06-13 14:02:39 -0600

    Made small changes to the CommunityEvents component responsiveness

--- app/components/Community/Events.vue
+++ app/components/Community/Events.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="my-4 space-y-8">
-    <li v-for="(event, idx) in events" :key="event.id" class="flex flex-col sm:flex-row md:max-w-[75%]">
+    <li v-for="(event, idx) in events" :key="event.id" class="flex flex-col sm:flex-row lg:max-w-[75%]">
       <!-- Left/Top Event (date) -->
       <div class="bg-brand-primary text-white font-bold text-2xl text-center py-3 sm:px-8 sm:flex sm:flex-col sm:justify-center">
         <div class="flex flex-col">
