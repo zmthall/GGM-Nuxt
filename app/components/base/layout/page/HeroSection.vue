79f3476fda0c1f5915776417cbe5a907cc0b904f
zmthall <zmthall@gmail.com>
2025-10-07 10:38:22 -0600

    Fixed fetch priority not working

--- app/components/base/layout/page/HeroSection.vue
+++ app/components/base/layout/page/HeroSection.vue
@@ -2,7 +2,7 @@
   <section class="relative h-[800px] max-xs:h-[950px] w-full">
     <div>
       <div class="absolute z-1 size-full bg-black/50" />
-      <NuxtImg :src format="avif" width="1280" height="720" :sizes="sizes" quality="40" class="absolute size-full object-cover z-0" fetchpriority="high" :preload="true" :alt :title :loading placeholder/>
+      <NuxtImg :src format="avif" width="1280" height="720" sizes="(max-width: 1024px) 100vw, 1280px" quality="40" class="absolute inset-0 w-full h-full object-cover z-0" :img-attrs="{ fetchpriority: 'high' }" :preload="{ fetchPriority: 'high' }" :alt :title loading="eager" />
     </div>
     <div :class="[{ 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2': centered }, styling, 'z-1 text-white max-sm:w-full sm:w-96 md:w-6/12 flex flex-col items-center']">
       <slot />
@@ -17,13 +17,9 @@ withDefaults(defineProps<{
   styling?: string;
   alt: string;
   title?: string;
-  loading?: "lazy" | "eager";
-  sizes?: string
 }>(), {
   styling: undefined,
   title: undefined,
-  loading: "lazy",
-  sizes: '(max-width: 640px) 100vw, (max-width: 1024px) 100vw, 1280px'
 })
 </script>
 
