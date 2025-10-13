d0a0967bf7d320539c948c8d0a52fdfaf71c5807
zmthall <zmthall@gmail.com>
2025-10-13 10:48:23 -0600

    Optimized LCP on non-medical even further

--- app/components/base/layout/page/Banner.vue
+++ app/components/base/layout/page/Banner.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="my-4 sm:my-8 mx-4 sm:mx-8 sm:rounded-lg sm:shadow-primary xl:max-w-[1200px] xl:mx-auto">
-        <NuxtImg :src :alt sizes="xl:1200px md:75vw sm:100vw" format="avif,webp" quality="60" :preload="{ fetchPriority: 'high' }" placeholder class="w-full sm:rounded-lg xl:object-cover xl:object-[10%] xl:h-[500px]" :class="aspectRatio" :height="imageHeight" :width="imageWidth" :title loading="eager" />
+        <NuxtImg :src :sizes :alt format="webp,avif" densities="1" quality="80" :preload="{ fetchPriority: 'high' }" placeholder class="w-full sm:rounded-lg xl:object-cover xl:object-[10%]" :class="aspectRatio" :height="imageHeight" :width="imageWidth" :title loading="eager" />
     </div>
 </template>
 
@@ -13,10 +13,12 @@ withDefaults(defineProps<{
     aspectRatio?: string;
     imageHeight: string;
     imageWidth: string;
+    sizes?: string;
 }>(), {
     src: undefined,
     alt: undefined,
-    aspectRatio: '4/3'
+    aspectRatio: '4/3',
+    sizes: undefined
 })
 
 </script>
