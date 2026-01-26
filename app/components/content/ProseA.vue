99b0148e35615c4d5f472de120b4cbc2776bdb9d
zmthall <zmthall@gmail.com>
2026-01-26 08:25:43 -0700

    Made it so all external links on blog posts open in a new tab and have rel noopener noreferrer

--- app/components/content/ProseA.vue
+++ app/components/content/ProseA.vue
@@ -1,13 +1,32 @@
 <template>
-  <a class="link">
+  <a v-bind="attrsWithoutRel" class="link" :rel="finalRel">
     <slot />
   </a>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+import { computed, useAttrs } from 'vue'
 
+defineOptions({ inheritAttrs: false })
+
+const attrs = useAttrs()
+
+const attrsWithoutRel = computed(() => {
+  const { rel, ...rest } = attrs as Record<string, unknown>
+  return rest
+})
+
+const finalRel = computed(() => {
+  const incoming = String(attrs.rel ?? '')
+  const tokens = new Set(incoming.split(/\s+/).filter(Boolean))
+
+  // always enforce security for target="_blank"
+  tokens.add('noopener')
+  tokens.add('noreferrer')
+
+  // if you NEVER want nofollow, uncomment:
+  tokens.delete('nofollow')
+
+  return Array.from(tokens).join(' ')
+})
 </script>
-
-<style>
-
-</style>
