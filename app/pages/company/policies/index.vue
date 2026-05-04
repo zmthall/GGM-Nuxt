596d270733c663bb724fb162346d5f7a5ae331bf
zmthall <zmthall@gmail.com>
2026-05-04 15:08:32 -0600

    Translated the company/policies page

--- app/pages/company/policies/index.vue
+++ app/pages/company/policies/index.vue
@@ -2,34 +2,29 @@
   <div>
     <BaseLayoutPageSection margin="pTop">
       <BaseLayoutPageContainer class="space-y-8">
-        <h2 class="text-2xl font-bold text-brand-primary max-sm:hidden">Golden Gate Manor Inc Website Policies</h2>
+        <h2 class="text-2xl font-bold text-brand-primary max-sm:hidden">{{ $t('company.policies.website.title') }}</h2>
         <div class="flex flex-col lg:grid lg:grid-cols-3 gap-8">
-          <NuxtLink to="/company/policies/terms-and-conditions" class="h-24 justify-center bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out flex gap-4 items-center group"><BaseIcon name="game-icons:scales" color="text-black" hover-color="group-hover:text-white" /><span>Terms and Conditions</span></NuxtLink>
-          <NuxtLink to="/company/policies/privacy" class="h-24 flex items-center gap-4 justify-center bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out group"><BaseIcon name="solar:shield-bold" color="text-black" hover-color="group-hover:text-white" /><span>Privacy Policy</span></NuxtLink>
-          <NuxtLink to="/company/policies/accessibility" class="h-24 flex items-center gap-4 justify-center bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out group"><BaseIcon name="ic:round-handshake" color="text-black" hover-color="group-hover:text-white" /><span>Accessibility</span></NuxtLink>
+          <BaseUiAction v-for="policy in websitePolicies" :key="`website-${$rt(policy.content)}`" :to="$localePath($rt(policy.to))" variant="blank" styling="flex gap-4 items-center justify-center h-24" class="bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out group"><BaseIcon :name="$rt(policy.icon)" color="text-black" hover-color="group-hover:text-white"/><span>{{ $rt(policy.content) }}</span></BaseUiAction>
         </div>
       </BaseLayoutPageContainer>
     </BaseLayoutPageSection>
     <BaseLayoutPageSection margin="default">
       <BaseLayoutPageContainer class="space-y-8">
-        <h2 class="text-2xl font-bold text-brand-primary">Transportation Policies</h2>
+        <h2 class="text-2xl font-bold text-brand-primary">{{ $t('company.policies.transportation.title') }}</h2>
         <div>
-          <NuxtLink to="/company/policies/ride-cancellation" class="h-24 flex items-center gap-4 justify-center bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out group"><BaseIcon name="material-symbols:event-busy-sharp" color="text-black" hover-color="group-hover:text-white" /><span>Ride Cancellation Policy</span></NuxtLink>
+          <NuxtLink :to="$localePath('/company/policies/ride-cancellation')" class="h-24 flex items-center gap-4 justify-center bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out group"><BaseIcon name="material-symbols:event-busy-sharp" color="text-black" hover-color="group-hover:text-white" /><span>{{ $t('company.policies.transportation.policy[0].content') }}</span></NuxtLink>
         </div>
-        <BaseUiAction to="/company/contact-us" class="p-4">Transportation Contact</BaseUiAction>
+        <BaseUiAction :to="$localePath('/company/contact-us')" class="p-4">{{ $t('company.policies.transportation.button') }}</BaseUiAction>
       </BaseLayoutPageContainer>
     </BaseLayoutPageSection>
     <DeferRender when="visible" root-margin="1px 0px">
       <BaseLayoutPageSection margin="pDefault" class="cv-auto">
         <BaseLayoutPageContainer class="space-y-8">
-          <h2 class="text-2xl font-bold text-brand-primary">Medical Ecommerce Policies</h2>
+          <h2 class="text-2xl font-bold text-brand-primary">{{ $t('company.policies.medical-ecommerce.title') }}</h2>
           <div class="flex flex-col lg:grid lg:grid-cols-4 gap-8">
-            <a href="https://goldengatemedicalsupply.com/pages/terms-conditions" target="_blank" rel="noopener" class="h-24 flex items-center gap-4 justify-center bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out group"><BaseIcon name="game-icons:scales" color="text-black" hover-color="group-hover:text-white" /><span>Terms and Conditions</span></a>
-            <a href="https://goldengatemedicalsupply.com/pages/privacy-policy" target="_blank" rel="noopener" class="h-24 flex items-center gap-4 justify-center bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out group"><BaseIcon name="solar:shield-bold" color="text-black" hover-color="group-hover:text-white" /><span>Privacy Policy</span></a>
-            <a href="https://goldengatemedicalsupply.com/pages/shipping-policy" target="_blank" rel="noopener" class="h-24 flex items-center gap-4 justify-center bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out group"><BaseIcon name="hugeicons:shipping-truck-02" color="text-black" hover-color="group-hover:text-white" /><span>Shipping Policy</span></a>
-            <a href="https://goldengatemedicalsupply.com/pages/return-policy" target="_blank" rel="noopener" class="h-24 flex items-center gap-4 justify-center bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out group"><BaseIcon name="tabler:truck-return" color="text-black" hover-color="group-hover:text-white" /><span>Return Policy</span></a>
+            <BaseUiAction v-for="policy in medicalEcommercePolicies" :key="`med-ecom-${$rt(policy.content)}`" new-page :href="$rt(policy.href)" variant="blank" styling="h-24 flex items-center gap-4 justify-center" class="bg-zinc-300 rounded-lg hover:bg-zinc-500 hover:text-white hover:font-bold lg:hover:scale-105 transition-transform duration-500 ease-in-out group"><BaseIcon :name="$rt(policy.icon)" color="text-black" hover-color="group-hover:text-white" /><span>{{ $rt(policy.content) }}</span></BaseUiAction>
           </div>
-          <BaseUiAction href="https://goldengatemedicalsupply.com/pages/contact-us" :new-page="true" class="p-4">Medical Supply Contact</BaseUiAction>
+          <BaseUiAction href="https://goldengatemedicalsupply.com/pages/contact-us" new-page class="p-4">{{ $t('company.policies.medical-ecommerce.button') }}</BaseUiAction>
         </BaseLayoutPageContainer>
       </BaseLayoutPageSection>
     </DeferRender>
@@ -63,6 +58,16 @@ useSeoMeta({
 defineOptions({
   name: 'CompanyPoliciesPage'
 })
+
+type Policies = {
+  content: string;
+  icon: string;
+  to: string;
+  href: string;
+}[]
+
+const websitePolicies = $tm('company.policies.website.policy') as Policies;
+const medicalEcommercePolicies = $tm('company.policies.medical-ecommerce.policy') as Policies;
 
 
 </script>
